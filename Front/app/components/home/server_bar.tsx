07c46a39b4cbb8857e9622f91f9892c2c5a1b1a4
jmitchell35 <julian.mitchell@epitech.eu>
2026-03-17 16:54:24 +0100

    feat(i18n) - translate hardcoded French strings in ServerBar

--- Front/app/components/home/server_bar.tsx
+++ Front/app/components/home/server_bar.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { HomeIcon } from "@heroicons/react/24/solid";
+import { useTranslation } from "react-i18next";
 
 type Props = {
     servers: Array<{ id: string; name: string; icon: string; notif?: number }>;
@@ -12,6 +13,7 @@ type Props = {
 };
 
 export function ServerBar({ servers, onAdd, onSelect, activeId }: Props) {
+    const { t } = useTranslation();
     return (
         <aside className="home-servers">
             <Link className="home-logo home-logo-btn" href="/home/dm" aria-label="Home">
@@ -34,13 +36,13 @@ export function ServerBar({ servers, onAdd, onSelect, activeId }: Props) {
             <div className="home-spacer" />
             <button
                 className="home-server-btn home-server-add"
-                title="Ajouter un serveur"
+                title={t('shell.add_server')}
                 onClick={onAdd}
             >
                 +
             </button>
             <div className="home-avatar">
-                <Image src="/avatar.png" alt="Moi" width={48} height={48} className="home-avatar-img" />
+                <Image src="/avatar.png" alt={t('common.me')} width={48} height={48} className="home-avatar-img" />
             </div>
         </aside>
     );
